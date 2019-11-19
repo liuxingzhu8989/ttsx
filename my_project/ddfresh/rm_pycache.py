e08255c4ceea2fcd33ca011d7c09c653dd1e0241
liuxingzhu898 <liuxingzhu8989@163.com>
2019-11-19 08:03:08 -0500

    update git

--- my_project/ddfresh/rm_pycache.py
+++
@@ -1,7 +0,0 @@
-import os
-import shutil
-for root, dirs, files in os.walk(os.getcwd(), topdown=True):
-    print(dirs)
-    for dir in dirs:
-        if dir == '__pycache__':
-            shutil.rmtree(dir)
