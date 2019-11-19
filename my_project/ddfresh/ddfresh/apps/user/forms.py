dd9336a3b33dd8deecdcc137908a1df3d1703ceb
liuxingzhu898 <liuxingzhu8989@163.com>
2019-11-19 07:43:38 -0500

    update git

--- my_project/ddfresh/ddfresh/apps/user/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class UserForm(forms.Form):
-    username = forms.CharField(max_length=100)
-    password = forms.CharField(max_length=100)
-    email = forms.EmailField()
-    
