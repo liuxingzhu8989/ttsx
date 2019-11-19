e7e4ba27fe1c5f49f80635ebd2b6fe16978fab81
liuxingzhu898 <liuxingzhu8989@163.com>
2019-11-19 09:29:37 -0500

    update git

--- my_project/ddfresh/db/models.py
+++ my_project/ddfresh/db/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class BaseModels(models.Model):
+class BaseModel(models.Model):
     create_time = models.DateTimeField(auto_now_add = True, verbose_name =' 添加时间')
     update_time = models.DateTimeField(auto_now = True, verbose_name = '更新时间')
     is_delete = models.BooleanField(default = False)
